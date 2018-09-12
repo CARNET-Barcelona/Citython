54890d7076b14051778506ea9a83aa89a991d8fa
Unknown <mgijon94@gmail.com>
2018-09-12 20:07:01 +0200

    updated

--- webpage/src/App.vue
+++ webpage/src/App.vue
@@ -1,15 +1,10 @@
 <template>
+  
   <div id="app">
     <router-view/>
   </div>
 
-<!--
-  <div class="footer">
-    cosas nazis peter, cosas nazis
 
-  </div>
-
--->
 </template>
 
 <script>
--- webpage/src/router/index.js
+++ webpage/src/router/index.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import HelloWorld from '@/components/HelloWorld'
+/*  cosas nuevas */
+import Agenda from 'components/Agenda'
+import Register from 'components/Register'
+import Contact from 'components/Contact'
+
 
 Vue.use(Router)
 
@@ -10,6 +16,22 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    /* cosas nuevas */
+    {
+    	path: 'components/',
+    	name: 'Agenda',
+    	component: Agenda
+    },
+    {
+    	path: 'components/',
+    	name: 'Register',
+    	component: Register
+    },
+    {
+    	path: 'components/',
+    	name: 'Contact',
+    	component: Contact
     }
   ]
 })
