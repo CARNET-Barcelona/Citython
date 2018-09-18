f0dd87812ec9c7d2c926ec9574858466b968e23f
Unknown <mgijon94@gmail.com>
2018-09-18 13:54:19 +0200

    updated

--- webpage/src/components/Register.vue
+++ webpage/src/components/Register.vue
@@ -1,8 +1,19 @@
 <template>
 	<div id="Register">
-		<div class="centrado">
-			<button class="Register-button">Sing Up!!</button>
-		</div> <!-- end centrado -->
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="centrado">
+						<a href="#"><button class="Register-button" id="Register-button-1">Sing Up!!</button></a>
+					</div>
+				</div> <!-- button for urbanis-cosa -->
+				<div class="col-md-6">
+					<div class="centrado">
+						<a href="#"><button class="Register-button" id="Register-button-2">Sing Up!!</button></a>
+					</div>
+				</div> <!-- button for data sicentits -->
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -10,6 +21,12 @@
 	export default {
 	  name: 'Register'
 	}
+
+
+	var boton1 = document.getElementById("Register-button-1")   // botón de registro para los uban-cosa
+	var boton2 = document.getElementById("Register-button-2")   // botón de registro para los data-scientits
+
+	// aquí deberé hacer algún tipo de animación chula que me sirva para entretenerme
 </script>
 
 <style>
@@ -17,12 +34,12 @@
 	display: block;
 }
 .centrado{
-	position: absolute;
+	position: relative;
 	display: block;
 	top: 50%;    /* puedo hacer que el botón se mueva de un lado para otro o algo así, quedaría chulo 
 	                con pompitas moviéndose por todos lados de color rosa, excepto cuando entren en contacto con
 	                el botón que serán azules */
-	left: 45%;
+	padding: 40px;
 }
 .Register-button{
 	background-color: pink;
