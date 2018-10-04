e2425b704868bcf4f08a4dd345a05539dd5dbecf
Unknown <mgijon94@gmail.com>
2018-10-04 18:01:48 +0200

    updated

--- web/src/main.js
+++ web/src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.use(VueRouter)
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
